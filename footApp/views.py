7d5b220eed6c0d75e41352ae38933d5e56441878
marine <dedola.marine@hotmail.com>
2020-10-20 14:32:37 +0200

    Initialisation bdd

--- footApp/views.py
+++ footApp/views.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 @app.route('/')
 def index():
@@ -8,3 +9,6 @@ def index():
 
 if __name__ == "__main__":
     app.run()
+
+
+
