689af669d43661498f48535e453242480b36325c
VanciaC <vancia.chan@ipilyon.net>
2020-10-21 14:57:32 +0200

    Ajouts des routes home, index, team

--- footApp/views.py
+++ footApp/views.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 @app.route('/')
 def index():
-    return "HomePage"
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
 
 if __name__ == "__main__":
     app.run()
