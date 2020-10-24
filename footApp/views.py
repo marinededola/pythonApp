562b111c43923919502a89d8a3ac482ec5341076
marine <dedola.marine@hotmail.com>
2020-10-24 13:13:06 +0200

    Récupération données bdd

--- footApp/views.py
+++ footApp/views.py
@@ -1,19 +1,39 @@
+import sqlite3
 from flask import Flask, render_template
 
+def get_db_connection():
+    conn = sqlite3.connect('footApp/database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 app = Flask(__name__)
-app.config.from_object('config')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    users = conn.execute('SELECT * FROM user').fetchall()
+    favoris = conn.execute('SELECT * FROM favoris').fetchall()
+    conn.close()
+    return render_template('index.html', users=users, favoris=favoris)
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    conn = get_db_connection()
+    teams = conn.execute('SELECT * FROM team').fetchall()
+    favoris = conn.execute('SELECT * FROM favoris').fetchall()
+    conn.close()
+    return render_template('home.html', teams=teams, favoris=favoris)
 
 @app.route('/team')
 def team():
-    return render_template('team.html')
+    conn = get_db_connection()
+    teams = conn.execute('SELECT * FROM team').fetchall()
+    favoris = conn.execute('SELECT * FROM favoris').fetchall()
+    conn.close()
+    return render_template('team.html', teams=teams, favoris=favoris)
+
+
+app.config.from_object('config')
 
 if __name__ == "__main__":
     app.run()
