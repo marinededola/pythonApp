84599942ca635affae8bb91c01eae21bfabf54f3
marine <dedola.marine@hotmail.com>
2020-11-02 00:22:09 +0100

    Affichage des matchs du jour

--- footApp/views.py
+++ footApp/views.py
@@ -1,6 +1,24 @@
 import sqlite3
+import requests
+import datetime
 from flask import Flask, render_template
 
+#Pick the current date
+date = datetime.datetime.now()
+#Adjust the date's format to the API
+    dateDay= str(date.day)
+jourUrl = str(date.year) + "-" + str(date.month) + "-" + dateDay
+
+#Initialize the football API's URL
+url = "https://stroccoli-futbol-science-v1.p.rapidapi.com/s1/calendar/" + jourUrl + "/" + jourUrl
+
+#Initialize query parameters
+headers = {
+'x-rapidapi-host': "stroccoli-futbol-science-v1.p.rapidapi.com",
+'x-rapidapi-key': "3c3e50e2d3msh35e5bd40a835711p1680e1jsn6f0ff3f70e35"
+}
+
+#Connexion to the database
 def get_db_connection():
     conn = sqlite3.connect('footApp/database.db')
     conn.row_factory = sqlite3.Row
@@ -8,6 +26,7 @@ def get_db_connection():
 
 app = Flask(__name__)
 
+#Initialization of application routes
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -22,7 +41,9 @@ def home():
     teams = conn.execute('SELECT * FROM team').fetchall()
     favoris = conn.execute('SELECT * FROM favoris').fetchall()
     conn.close()
-    return render_template('home.html', teams=teams, favoris=favoris)
+    #Football API request
+    match = requests.request("GET", url, headers=headers).json()
+    return render_template('home.html', teams=teams, favoris=favoris, match=match)
 
 @app.route('/team')
 def team():
@@ -30,8 +51,9 @@ def team():
     teams = conn.execute('SELECT * FROM team').fetchall()
     favoris = conn.execute('SELECT * FROM favoris').fetchall()
     conn.close()
-    return render_template('team.html', teams=teams, favoris=favoris)
-
+    #Football API request
+    match = requests.request("GET", url, headers=headers).json()
+    return render_template('team.html', teams=teams, favoris=favoris, match=match)
 
 app.config.from_object('config')
 
