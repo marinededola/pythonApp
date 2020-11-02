d0f3efb348586565ece22d19fc96770acdf7fbc9
VanciaC <vancia.chan@ipilyon.net>
2020-11-02 22:06:16 +0100

    Ajout macro formulaire + register formulaire, view error

--- footApp/views.py
+++ footApp/views.py
@@ -2,7 +2,7 @@ import sqlite3
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Length
+from wtforms.validators import InputRequired, Length, DataRequired, EqualTo
 from werkzeug.security import generate_password_hash, check_password_hash
 
 def get_db_connection():
@@ -14,32 +14,34 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thesecretkey'
 
 class LoginForm(FlaskForm):
-    pseudo = StringField('pseudo', validators=[InputRequired('Un pseudo est requis'), Length(min=4, max=25, message='Le pseudo doit êtr entre 4 et 25 caractères.')])
-    password = PasswordField('password', validators=[InputRequired('Un mot de passe est requis'), Length(min=6, max=32, message='Le mot de passe doit être entre 6 et 32 caractères.')])
+    pseudo = StringField('Pseudo', validators=[InputRequired('Un pseudo est requis'), Length(min=4, max=25, message='Le pseudo doit êtr entre 4 et 25 caractères.')])
+    password = PasswordField('Mot de passe', validators=[InputRequired('Un mot de passe est requis'), Length(min=6, max=32, message='Le mot de passe doit être entre 6 et 32 caractères.')])
 
 class RegisterForm(FlaskForm):
-    pseudo = StringField('pseudo', validators=[InputRequired(), Length(min=4, max=25)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=32)])
+    pseudo = StringField('Pseudo', validators=[InputRequired(), Length(min=4, max=25)])
+    password = PasswordField('Mot de passe', validators=[InputRequired(), Length(min=6, max=32)])
+    confirm_password = PasswordField('Confirmation de votre mot de passe', validators=[DataRequired(), EqualTo('password')])
 
 @app.route('/')
 def index():
-    form = LoginForm()
+    loginForm = LoginForm()
+    registerForm = RegisterForm()
     conn = get_db_connection()
     users = conn.execute('SELECT * FROM user').fetchall()
-    favoris = conn.execute('SELECT * FROM favoris').fetchall()
     conn.close()
-    return render_template('index.html', users=users, favoris=favoris, form=form)
+    return render_template('index.html', users=users, loginForm=loginForm, registerForm=registerForm)
 
 @app.route('/home', methods=['get', 'post'])
 def home():
-    form = LoginForm()
+    loginForm = LoginForm()
+    registerForm = RegisterForm()
     conn = get_db_connection()
     teams = conn.execute('SELECT * FROM team').fetchall()
     favoris = conn.execute('SELECT * FROM favoris').fetchall()
     conn.close()
-    if form.validate_on_submit():
-        return render_template('home.html', teams=teams, favoris=favoris, form=form)
-    return render_template('index.html', form=form)
+    if loginForm.validate_on_submit():
+        return render_template('home.html', teams=teams, favoris=favoris, loginForm=loginForm)
+    return render_template('index.html', loginForm=loginForm, registerForm=registerForm)
 
 @app.route('/team')
 def team():
@@ -51,11 +53,13 @@ def team():
 
 @app.route('/register', methods=['get', 'post'])
 def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        return render_template('index.html', form=form)
+    loginForm = LoginForm()
+    registerForm = RegisterForm()
+    error = "Erreur sur l'un des champs !"
+    if registerForm.validate_on_submit():
+        return render_template('index.html', loginForm=loginForm, registerForm=registerForm)
     else:
-        return "error"
+        return render_template('index.html', loginForm=loginForm, registerForm=registerForm, error=error)
     
 
 app.config.from_object('config')
