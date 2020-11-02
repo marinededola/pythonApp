cd9198d35050ea76f0ef83216eabf87cf84985b5
VanciaC <vancia.chan@ipilyon.net>
2020-11-02 09:35:20 +0100

    Mise à jour du formulaire d'inscription - connexion/ Gestion des erreurs

--- footApp/views.py
+++ footApp/views.py
@@ -1,5 +1,9 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Length
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def get_db_connection():
     conn = sqlite3.connect('footApp/database.db')
@@ -7,22 +11,35 @@ def get_db_connection():
     return conn
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Thesecretkey'
+
+class LoginForm(FlaskForm):
+    pseudo = StringField('pseudo', validators=[InputRequired('Un pseudo est requis'), Length(min=4, max=25, message='Le pseudo doit êtr entre 4 et 25 caractères.')])
+    password = PasswordField('password', validators=[InputRequired('Un mot de passe est requis'), Length(min=6, max=32, message='Le mot de passe doit être entre 6 et 32 caractères.')])
+
+class RegisterForm(FlaskForm):
+    pseudo = StringField('pseudo', validators=[InputRequired(), Length(min=4, max=25)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=32)])
 
 @app.route('/')
 def index():
+    form = LoginForm()
     conn = get_db_connection()
     users = conn.execute('SELECT * FROM user').fetchall()
     favoris = conn.execute('SELECT * FROM favoris').fetchall()
     conn.close()
-    return render_template('index.html', users=users, favoris=favoris)
+    return render_template('index.html', users=users, favoris=favoris, form=form)
 
-@app.route('/home')
+@app.route('/home', methods=['get', 'post'])
 def home():
+    form = LoginForm()
     conn = get_db_connection()
     teams = conn.execute('SELECT * FROM team').fetchall()
     favoris = conn.execute('SELECT * FROM favoris').fetchall()
     conn.close()
-    return render_template('home.html', teams=teams, favoris=favoris)
+    if form.validate_on_submit():
+        return render_template('home.html', teams=teams, favoris=favoris, form=form)
+    return render_template('index.html', form=form)
 
 @app.route('/team')
 def team():
@@ -32,6 +49,14 @@ def team():
     conn.close()
     return render_template('team.html', teams=teams, favoris=favoris)
 
+@app.route('/register', methods=['get', 'post'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return render_template('index.html', form=form)
+    else:
+        return "error"
+    
 
 app.config.from_object('config')
 
