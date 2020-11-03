01b96d638346e3050c6faff89cc11f46b6e754d4
VanciaC <vancia.chan@ipilyon.net>
2020-11-03 21:12:22 +0100

    Formulaire d'inscription, gestion des erreurs, ajout d'un nouvelle utilisateur, mot de passe hash

--- footApp/views.py
+++ footApp/views.py
@@ -14,11 +14,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thesecretkey'
 
 class LoginForm(FlaskForm):
-    pseudo = StringField('Pseudo', validators=[InputRequired('Un pseudo est requis'), Length(min=4, max=25, message='Le pseudo doit êtr entre 4 et 25 caractères.')])
+    username = StringField('Pseudo', validators=[InputRequired('Un pseudo est requis'), Length(min=4, max=25, message='Le pseudo doit êtr entre 4 et 25 caractères.')])
     password = PasswordField('Mot de passe', validators=[InputRequired('Un mot de passe est requis'), Length(min=6, max=32, message='Le mot de passe doit être entre 6 et 32 caractères.')])
 
 class RegisterForm(FlaskForm):
-    pseudo = StringField('Pseudo', validators=[InputRequired(), Length(min=4, max=25)])
+    username = StringField('Pseudo', validators=[InputRequired(), Length(min=4, max=25)])
     password = PasswordField('Mot de passe', validators=[InputRequired(), Length(min=6, max=32)])
     confirm_password = PasswordField('Confirmation de votre mot de passe', validators=[DataRequired(), EqualTo('password')])
 
@@ -28,7 +28,9 @@ def index():
     registerForm = RegisterForm()
     conn = get_db_connection()
     users = conn.execute('SELECT * FROM user').fetchall()
-    conn.close()
+    for user in users:
+        print(user[1])
+    conn.close
     return render_template('index.html', users=users, loginForm=loginForm, registerForm=registerForm)
 
 @app.route('/home', methods=['get', 'post'])
@@ -38,9 +40,13 @@ def home():
     conn = get_db_connection()
     teams = conn.execute('SELECT * FROM team').fetchall()
     favoris = conn.execute('SELECT * FROM favoris').fetchall()
-    conn.close()
-    if loginForm.validate_on_submit():
-        return render_template('home.html', teams=teams, favoris=favoris, loginForm=loginForm)
+    if request.method == 'POST':
+        username_field = request.form.get('username')
+        password_field = request.form.get('password')
+        password_hash = generate_password_hash(password_field)
+        if loginForm.validate_on_submit():
+            print(password_field, username_field, password_hash)
+            return render_template('home.html', teams=teams, favoris=favoris, loginForm=loginForm)
     return render_template('index.html', loginForm=loginForm, registerForm=registerForm)
 
 @app.route('/team')
@@ -55,12 +61,28 @@ def team():
 def register():
     loginForm = LoginForm()
     registerForm = RegisterForm()
-    error = "Erreur sur l'un des champs !"
-    if registerForm.validate_on_submit():
-        return render_template('index.html', loginForm=loginForm, registerForm=registerForm)
-    else:
-        return render_template('index.html', loginForm=loginForm, registerForm=registerForm, error=error)
-    
+    conn = get_db_connection()
+    if request.method == 'POST':
+        try:
+            if registerForm.validate_on_submit():
+                username_field = request.form['username']
+                password_field = request.form['password']
+                password_hash = generate_password_hash(password_field)
+                msg = "Inscription réussie !"
+                conn.execute('INSERT INTO user (username, password) VALUES (?, ?)',
+                (username_field, password_hash))
+                conn.commit()
+                conn.close()
+                return render_template('index.html', loginForm=loginForm, registerForm=registerForm, msg=msg)
+            else:
+                msg = "Une erreur est survenue !"
+                return render_template('index.html', loginForm=loginForm, registerForm=registerForm, msg=msg)
+        except:
+            msg = "Identifiant déja utilisé !"
+            return render_template('index.html', loginForm=loginForm, registerForm=registerForm, msg=msg)
+            
+        
+    return render_template('index.html', loginForm=loginForm, registerForm=registerForm)
 
 app.config.from_object('config')
 
